e0bf9455cae7e3f7d1af720ab83bc696633d4a3b
William Sharp <wsharp@ltu.edu>
2016-11-23 21:51:45 -0500

    updating the run.sh to compile all files

--- run.sh
+++ run.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-CLASSPATH=out/production/pcprox-java-sample:lib/*
+OUTPUTPATH=out/production/pcprox-java-sample
+CLASSPATH=out/production/pcprox-java-sample:lib/jna.jar
 APPNAME=App
 LIBPATH=./lib
 SRCPATH=src
 
-mkdir -p $CLASSPATH
-#javac -d $CLASSPATH $SRCPATH/$APPNAME.java
+mkdir -p $OUTPUTPATH
+javac -d $OUTPUTPATH -cp $CLASSPATH $SRCPATH/*.java
 
 # If you don't use LD_PRELOAD you will get hid errors thrown from pcProxAPI
 LD_PRELOAD=$LIBPATH/libhidapi-hidraw.so java -Djava.library.path=$LIBPATH -Djna.library.path=$LIBPATH -classpath $CLASSPATH $APPNAME
